923be67a19105a32a3c10ab4ee3d0e95092c1da8
IllusionCIS7 <illusion1802@outlook.de>
2021-10-05 21:12:48 +0200

    Zwischenstand

---
+++ Groups/src/illusioncis7/groups/Listener/ChatListener.java
@@ -0,0 +1,14 @@
+package illusioncis7.groups.Listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class ChatListener implements Listener {
+
+    @EventHandler
+    public void onChatEvent(AsyncPlayerChatEvent e)
+    {
+
+    }
+}
--- Groups/src/illusioncis7/groups/main/Main.java
+++ Groups/src/illusioncis7/groups/main/Main.java
@@ -1,7 +1,20 @@
 package illusioncis7.groups.main;
 
+import illusioncis7.groups.Listener.ChatListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
+    @Override
+    public void onEnable()
+    {
+        Bukkit.getPluginManager().registerEvents(new ChatListener(), this);
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
 }
