08ce6ed814c9973d083281793f2d6daa91f77d77
IllusionCIS7 <illusion1802@outlook.de>
2021-10-26 22:44:38 +0200

    EventListener registriert

--- Groups/src/illusioncis7/groups/main/Main.java
+++ Groups/src/illusioncis7/groups/main/Main.java
@@ -2,6 +2,7 @@ package illusioncis7.groups.main;
 
 import illusioncis7.groups.Listener.CLgroups;
 import illusioncis7.groups.Listener.ChatListener;
+import illusioncis7.groups.Listener.EventListener;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,6 +13,7 @@ public class Main extends JavaPlugin {
     public void onEnable()
     {
         Bukkit.getPluginManager().registerEvents(new ChatListener(), this);
+        Bukkit.getPluginManager().registerEvents(new EventListener(), this);
         Bukkit.getPluginCommand("groups").setExecutor(new CLgroups());
         Bukkit.getLogger().info(ChatColor.GREEN + "Groups ist aktiv!");
     }
