0a1336692d066899f4a44a089d448e267b84c476
IllusionCIS7 <illusion1802@outlook.de>
2021-10-26 22:25:56 +0200

    Methoden zum prüfen auf Gruppennamen und User hinzugefügt

--- Groups/src/illusioncis7/utils/Checker.java
+++ Groups/src/illusioncis7/utils/Checker.java
@@ -1,8 +1,12 @@
 package illusioncis7.utils;
 
+import illusioncis7.groups.main.CFG;
+import illusioncis7.groups.main.Group;
+import illusioncis7.groups.main.User;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import java.util.List;
+import java.util.Set;
 
 public final class Checker {
 
@@ -18,6 +22,40 @@ public final class Checker {
     {
         List<String> forbiddenNames = cm.getStringList("forbiddenGroupNames");
         return checkStrings(forbiddenNames, s);
+    }
+
+    public static Group checkGroupNameAvailability(String groupName)
+    {
+        FileConfiguration cm = new ConfigManager("groups.yml").getFileConfiguration();
+        Set<String> guids = null;
+        guids = cm.getConfigurationSection("groups").getKeys(false);
+        for (String guid : guids)
+        {
+            String gn = cm.getString(CFG.GroupName(guid));
+            if (gn.equalsIgnoreCase(groupName))
+            {
+                return new Group(guid);
+            }
+        }
+        return new Group(null);
+    }
+
+    public static User checkUserAvailability(String tcUUID)
+    {
+        FileConfiguration cm = new ConfigManager("user.yml").getFileConfiguration();
+        Set<String> uuids = null;
+        uuids = cm.getConfigurationSection("user").getKeys(false);
+        for (String uuid : uuids)
+        {
+            if (uuid.equals(tcUUID))
+            {
+                return new User(uuid);
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 
     private static boolean checkStrings(List<String> illegalStrings, String stc)
